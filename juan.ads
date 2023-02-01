0bdd328d7c00f3279816c7ec89d69d5da93aa02e
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-02-01 16:58:04 +0100

    Update juan.ads

--- juan.ads
+++ juan.ads
@@ -5,8 +5,8 @@
 
 			"NetworkAds": "admob",
 
-			"BannerAdmob": "ca-app-pub-1010770342437944/6509694722",
-			"InterstitialAdmob": "ca-app-pub-1010770342437944/7132495340",
+			"BannerAdmob": "ca-app-pub-1010770342437944/9844258903",
+			"InterstitialAdmob": "ca-app-pub-1010770342437944/5544538359",
 
 			"BannerFacebook": "628915755243490_628921531909579",
 			"InterstitialFacebook": "628915755243490_628921761909556",
